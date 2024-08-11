779021ea8777182950fd454881ee00a620542163
LSG <sukyeng97@naver.com>
2024-08-11 10:28:43 +0900

    test

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+const port = process.env.PORT || 3000; //
+app.listen(port);
+
+app.get("/api/lunch", (req, res) =>
+  res.json({ menu: "떡볶이", store: "신전떡볶이", price: 15000 })
+);
+
+app.get("/api/dinner", (req, res) => {
+  res.json({ menu: "수원왕갈비", store: "집앞", price: 11900 });
+});
